328925336bd30c5efa2d9be930c045987336fb5e
chenshan <chenshan@localhost>
2018-02-23 22:43:15 +0800

    版本更新

--- src/main/java/com/cdkj/loan/APIServlet.java
+++ src/main/java/com/cdkj/loan/APIServlet.java
@@ -42,7 +42,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:v1.0.0 \n");
-        writer.append("Description:loan_6th \n");
+        writer.append("Description:loan_6。1th \n");
         writer.flush();
     }
 
